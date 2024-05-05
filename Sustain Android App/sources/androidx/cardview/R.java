8169f2251e42f5a4d255e22bd465435b42cd6412
Sajan <75510698+SajanBrar@users.noreply.github.com>
2024-05-05 07:10:59 +0530

    Android app

---
+++ Sustain Android App/sources/androidx/cardview/R.java
@@ -0,0 +1,74 @@
+package androidx.cardview;
+
+public final class R {
+
+    public static final class attr {
+        public static int cardBackgroundColor = 2130903128;
+        public static int cardCornerRadius = 2130903129;
+        public static int cardElevation = 2130903130;
+        public static int cardMaxElevation = 2130903131;
+        public static int cardPreventCornerOverlap = 2130903132;
+        public static int cardUseCompatPadding = 2130903133;
+        public static int cardViewStyle = 2130903134;
+        public static int contentPadding = 2130903190;
+        public static int contentPaddingBottom = 2130903191;
+        public static int contentPaddingLeft = 2130903192;
+        public static int contentPaddingRight = 2130903193;
+        public static int contentPaddingTop = 2130903194;
+
+        private attr() {
+        }
+    }
+
+    public static final class color {
+        public static int cardview_dark_background = 2131034153;
+        public static int cardview_light_background = 2131034154;
+        public static int cardview_shadow_end_color = 2131034155;
+        public static int cardview_shadow_start_color = 2131034156;
+
+        private color() {
+        }
+    }
+
+    public static final class dimen {
+        public static int cardview_compat_inset_shadow = 2131099731;
+        public static int cardview_default_elevation = 2131099732;
+        public static int cardview_default_radius = 2131099733;
+
+        private dimen() {
+        }
+    }
+
+    public static final class style {
+        public static int Base_CardView = 2131689483;
+        public static int CardView = 2131689635;
+        public static int CardView_Dark = 2131689636;
+        public static int CardView_Light = 2131689637;
+
+        private style() {
+        }
+    }
+
+    public static final class styleable {
+        public static int[] CardView = {16843071, 16843072, com.mentalsupport.sustain.R.attr.cardBackgroundColor, com.mentalsupport.sustain.R.attr.cardCornerRadius, com.mentalsupport.sustain.R.attr.cardElevation, com.mentalsupport.sustain.R.attr.cardMaxElevation, com.mentalsupport.sustain.R.attr.cardPreventCornerOverlap, com.mentalsupport.sustain.R.attr.cardUseCompatPadding, com.mentalsupport.sustain.R.attr.contentPadding, com.mentalsupport.sustain.R.attr.contentPaddingBottom, com.mentalsupport.sustain.R.attr.contentPaddingLeft, com.mentalsupport.sustain.R.attr.contentPaddingRight, com.mentalsupport.sustain.R.attr.contentPaddingTop};
+        public static int CardView_android_minHeight = 1;
+        public static int CardView_android_minWidth = 0;
+        public static int CardView_cardBackgroundColor = 2;
+        public static int CardView_cardCornerRadius = 3;
+        public static int CardView_cardElevation = 4;
+        public static int CardView_cardMaxElevation = 5;
+        public static int CardView_cardPreventCornerOverlap = 6;
+        public static int CardView_cardUseCompatPadding = 7;
+        public static int CardView_contentPadding = 8;
+        public static int CardView_contentPaddingBottom = 9;
+        public static int CardView_contentPaddingLeft = 10;
+        public static int CardView_contentPaddingRight = 11;
+        public static int CardView_contentPaddingTop = 12;
+
+        private styleable() {
+        }
+    }
+
+    private R() {
+    }
+}
